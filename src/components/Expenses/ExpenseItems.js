1aab674868c03f10786388ae3bb83858ddee9828
vedatozayar <vedatozayar@gmail.com>
2022-08-20 01:15:09 +0300

    inital commit

---
+++ src/components/Expenses/ExpenseItems.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./ExpenseItems.css";
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
+
+const ExpenseItems = (props) => {
+  return (
+    <li>
+      <Card className="expense-item">
+        <ExpenseDate date={props.date} />
+        <div className="expense-item__description">
+          <h2>{props.title}</h2>
+          <div className="expense-item__price">{props.amount}€</div>
+        </div>
+      </Card>
+    </li>
+  );
+};
+
+export default ExpenseItems;
---
+++ src/components/Expenses/ExpensesList.js
@@ -0,0 +1,25 @@
+import ExpenseItems from "./ExpenseItems";
+import React from "react";
+import "./ExpensesList.css";
+const ExpensesList = (props) => {
+  let expensesContent = <p>No expenses found.</p>;
+
+  if (props.items.length === 0) {
+    return <h2 className="expenses-list__fallback">Found no expenses</h2>;
+  }
+
+  return (
+    <ul className="expenses-list">
+      {props.items.map((expense) => (
+        <ExpenseItems
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default ExpensesList;
